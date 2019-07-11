b3cde27387d7f404c0600d4d12ae57eade5c734a
baddiecoder <lawrenceo@fb.com>
2019-07-11 11:31:33 -0700

    Adding Compose fragment

--- app/src/main/java/com/codepath/gram/HomeActivity.java
+++ app/src/main/java/com/codepath/gram/HomeActivity.java
@@ -1,11 +1,13 @@
 package com.codepath.gram;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.codepath.gram.Fragments.ComposeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -20,28 +22,37 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         bottomNavigationView =  findViewById(R.id.bottom_navigation);
 
+        final FragmentManager fragmentManager = getSupportFragmentManager();
 
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+               Fragment fragment = new ComposeFragment();
                 switch (item.getItemId()) {
                     case R.id.action_compose:
-                        Intent intent = new Intent(HomeActivity.this, ComposeFragment.class);
-                        startActivity(intent);
-                        return true;
+                      fragment  = new ComposeFragment();
+                        Toast. makeText(HomeActivity.this, "Compose",Toast.LENGTH_SHORT).show();
+                        break;
                     case R.id.action_profile:
-                        // do something here
-                        return true;
+                        fragment   = new ComposeFragment();
+                        Toast. makeText(HomeActivity.this, "profile",Toast.LENGTH_SHORT).show();
+                        break;
                     case R.id.action_home:
-                        // do something here
-                        return true;
-                    default: return true;
+                        fragment   = new ComposeFragment();
+                        Toast. makeText(HomeActivity.this, "home",Toast.LENGTH_SHORT).show();
+                        break;
+                    default: break;
                 }
+
+            fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+            return true;
+
             }
+
         });
 
-
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
 
     }
 
